3261955c6ef2b03c81cac9e09f5403ffa99e5dbf
zhufengnodejs <zhufengnodejs@126.com>
2016-08-03 10:23:08 +0800

    修改文章

--- routes/article.js
+++ routes/article.js
@@ -17,18 +17,35 @@ router.get('/add',auth.checkLogin,function(req,res){
 });
 
 router.post('/add',auth.checkLogin,function(req,res){
-   var article = req.body;
-   //从session中获取当前会话用户的ID
-   article.user = req.session.user._id;
-   Model('Article').create(article,function(err,doc){
-       if(err){
-           req.flash('error','发表文章失败');
-           res.redirect('back');
-       }else{
-           req.flash('success','发表文章成功');
-           res.redirect('/');
-       }
-   })
+   var article = req.body;//得到请求体
+   var _id = article._id;//先得到文章的ID
+   if(_id){//修改
+       Model('Article').update(//使用修改器修改对应文章的标题和内容
+           {_id:_id},
+           {$set:{title:article.title,content:article.content}},
+       function(err,result){
+           if(err){
+               req.flash('error','修改文章失败');
+               res.redirect('back');
+           }else{
+               req.flash('success','修改文章成功');
+               //如果前台是通过ajax请求此路由，那么并不能跳转
+               res.redirect('/article/detail/'+_id);
+           }
+       });
+   }else{//新增加
+       //从session中获取当前会话用户的ID
+       article.user = req.session.user._id;
+       Model('Article').create(article,function(err,doc){
+           if(err){
+               req.flash('error','发表文章失败');
+               res.redirect('back');
+           }else{
+               req.flash('success','发表文章成功');
+               res.redirect('/');
+           }
+       })
+   }
 });
 
 router.get('/detail/:_id',function(req,res){
@@ -40,6 +57,7 @@ router.get('/detail/:_id',function(req,res){
 router.get('/delete/:_id',function(req,res){
     Model('Article').findById(req.params._id,function(err,doc){
         if(doc){
+            console.log(req.session.user._id , doc.user);
             if(req.session.user._id == doc.user){
                 Model('Article').remove({_id:req.params._id},function(err,result){
                     res.redirect('/');
@@ -54,5 +72,15 @@ router.get('/delete/:_id',function(req,res){
     })
 
 });
+//先得到文章的ID，然后得到文章的详情对象，并跳转到编辑页(复用新增文章模板)
+router.get('/edit/:_id',function(req,res){
+  //得到文章的ID
+  var _id = req.params._id;
+  //根据ID查询文章对象，并把此对象赋给模板变量
+  Model('Article').findById(_id,function(err,doc){
+      res.render('article/add.html',{title:'修改文章',article:doc});
+  })
+});
+
 
 module.exports = router;
